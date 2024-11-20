4004a9c25fd5cc32d92311a99ed4cb9bc7886003
Ny19SzilvásiK <Ny19SzilvsiK@202-15>
2024-11-20 12:59:31 +0100

    Add project files.

---
+++ PalinkaSZK/Palinka.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+    using System.ComponentModel.DataAnnotations;
+
+namespace PalinkaSZK
+    {
+        internal class Palinka
+        {  
+            [Required]
+            private string? gyumolcs;
+            public int AlkoholFok { get; set; }
+            public string? Gyumolcs {
+                get => gyumolcs;
+                set {
+                    if (value is null) throw new Exception(
+                    "A gyümölcs értéke nem lehet null");
+
+                    if (value.Length < 3 || value.Length > 20) throw new Exception(
+                        $"Az érték, amit szeretnél beállítani a gyümölcsre: {value}\n" +
+                        $"Ennek a hossza: {value.Length}.\n A gyümölcs hossza csak [3, 20] között lehet!");
+
+                    gyumolcs = value;
+                }
+            }
+            public int Mennyiseg {  get; set; }
+            public int KeszitesEv { get; set; }
+            public int AlkoholAr { get; set; }
+
+            public Palinka(int alkoholFok, string gyumolcs, int mennyiseg, int keszitesEv, int alkoholAr)
+            {
+                AlkoholFok = alkoholFok;
+                Gyumolcs = gyumolcs;
+                Mennyiseg = mennyiseg;
+                KeszitesEv = keszitesEv;
+                AlkoholAr = alkoholAr;
+            }
+
+            public override string ToString()
+            {
+                return $"Alkohol fok: {AlkoholFok}\nGyümölcs: {Gyumolcs}\nMennyiség: {Mennyiseg} deciliter\nKészítés éve: {KeszitesEv}\nÁr: {AlkoholAr} ft/deciliter\n\n";
+            }
+        }
+    }
---
+++ PalinkaSZK/Program.cs
@@ -0,0 +1,37 @@
+﻿using PalinkaSZK;
+
+List<Palinka> palinkaList = [];
+string[] fruit = ["szilva", "barack", "dió", "körte", "meggy","alma","narancs","cseresznye","füge"];
+
+for (int i = 0; i < 20; i++)
+{
+    palinkaList.Add(new(
+        alkoholFok: Random.Shared.Next(30, 88),
+        gyumolcs: fruit[Random.Shared.Next(fruit.Length)],
+        mennyiseg: Random.Shared.Next(1, 51),
+        keszitesEv: Random.Shared.Next(2000, DateTime.Now.Year + 1),
+        alkoholAr: Random.Shared.Next(50,1001)));
+};
+
+var mennyisegLiter = palinkaList.Sum(x => (double)x.Mennyiseg / 10);
+Console.WriteLine($"Összesen {mennyisegLiter:00.0} liter pálinka van!\n\n");
+
+int vegsoAr = 0;
+
+for (int i = 0; i < 50; i++)
+{
+    int szamlal = i + 1;
+    var randomPalinka = palinkaList[Random.Shared.Next(palinkaList.Count)];
+    var mennyisegFele = randomPalinka.Mennyiseg / 2;
+    var teljesAr = mennyisegFele * randomPalinka.AlkoholAr;
+    
+    vegsoAr += teljesAr;
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"PÁLINKA COUNT: {szamlal}");
+    Console.ResetColor();
+    Console.WriteLine($"{randomPalinka}Ennyit iszol: {mennyisegFele} deciliter\nTeljes ár: {teljesAr} forint\n\n");
+}
+
+Console.WriteLine($"\nEz összesen: {vegsoAr} forint!");
+    
